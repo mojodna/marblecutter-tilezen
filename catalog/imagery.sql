b1eeb45ecb1d284848b37976f7d365f983c2325b
Seth Fitzsimmons <seth@mojodna.net>
2017-12-11 11:06:36 -0800

    Update schema

--- catalog/imagery.sql
+++ catalog/imagery.sql
@@ -6,13 +6,13 @@ CREATE TABLE imagery (
     url text,
     geom geometry(MultiPolygon, 4326),
     resolution double precision,
-    band integer,
     approximate_zoom integer,
     min_zoom integer,
     max_zoom integer,
     priority integer DEFAULT 0,
     enabled boolean NOT NULL DEFAULT TRUE,
-    meta jsonb
+    meta jsonb,
+    bands jsonb
 );
 
 -- when bands are tracked separately, this needs to change
