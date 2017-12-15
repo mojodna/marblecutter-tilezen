912467bb335296ede1a280a5c8706002cab219f7
Ian Dees <ian.dees@gmail.com>
2017-12-15 15:07:45 -0600

    Add Batch-related scripts from marblecutter's tilezen branch

--- examples/render_pyramid.py
+++ examples/render_pyramid.py
@@ -1,39 +1,60 @@
+# noqa
 # coding=utf-8
 from __future__ import print_function
 
 import argparse
+import hashlib
 import logging
 import os
+import random
 import time
 from functools import wraps
 from multiprocessing.dummy import Pool
 
-import boto3
-import botocore
-import mercantile
-from mercantile import Tile
 from shapely import wkb
 from shapely.geometry import box
 
+import boto3
+import botocore
+import mercantile
 import psycopg2
 import psycopg2.extras
+import threading
 from marblecutter import tiling
-from marblecutter.formats.geotiff import GeoTIFF
-from marblecutter.formats.png import PNG
+from marblecutter.formats import PNG, GeoTIFF
+from marblecutter.sources import MemoryAdapter
 from marblecutter.stats import Timer
-from tilezen.catalogs import MemoryCatalog
-from tilezen.transformations import Normal, Terrarium
+from marblecutter.transformations import Normal, Terrarium
+from mercantile import Tile
 
 logging.basicConfig(level=logging.INFO)
 # Quieting boto messages down a little
 logging.getLogger('boto3.resources.action').setLevel(logging.WARNING)
 logging.getLogger('botocore').setLevel(logging.WARNING)
+logging.getLogger('marblecutter').setLevel(logging.WARNING)
 logging.getLogger('marblecutter.mosaic').setLevel(logging.WARNING)
+logging.getLogger('marblecutter.sources').setLevel(logging.WARNING)
 logger = logging.getLogger('batchtiler')
 
-MAX_ZOOM = 15
+if os.environ.get('VERBOSE'):
+    logger.setLevel(logging.DEBUG)
+
+
+THREAD_LOCAL = threading.local()
+
+
+def initialize_thread():
+    # Each thread needs its own boto3 Session object - it's not threadsafe
+    THREAD_LOCAL.session = boto3.session.Session()
+    THREAD_LOCAL.s3 = THREAD_LOCAL.session.resource('s3')
+
+
 POOL_SIZE = 12
-POOL = Pool(POOL_SIZE)
+POOL = Pool(POOL_SIZE, initializer=initialize_thread)
+OVERWRITE = os.environ.get('OVERWRITE_EXISTING_OBJECTS') == 'true'
+# Only render these tile types
+ONLY_RENDER = os.environ.get('ONLY_RENDER').split(',') \
+              if os.environ.get('ONLY_RENDER') else None
 
 GEOTIFF_FORMAT = GeoTIFF()
 PNG_FORMAT = PNG()
@@ -41,90 +62,71 @@ NORMAL_TRANSFORMATION = Normal()
 TERRARIUM_TRANSFORMATION = Terrarium()
 
 RENDER_COMBINATIONS = [
-    ("normal", NORMAL_TRANSFORMATION, PNG_FORMAT, ".png"),
-    ("terrarium", TERRARIUM_TRANSFORMATION, PNG_FORMAT, ".png"),
-    ("geotiff", None, GEOTIFF_FORMAT, ".tif"),
+    ("normal", NORMAL_TRANSFORMATION, PNG_FORMAT, ".png", 1),
+    ("terrarium", TERRARIUM_TRANSFORMATION, PNG_FORMAT, ".png", 1),
+    ("geotiff", None, GEOTIFF_FORMAT, ".tif", 2),
 ]
 
 
-# From https://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
-def retry(ExceptionToCheck,
-          tries=4,
-          delay=3,
-          backoff=2,
-          max_delay=60,
-          logger=None):
-    def deco_retry(f):
-        @wraps(f)
-        def f_retry(*args, **kwargs):
-            mtries, mdelay = tries, delay
-            while mtries > 1:
-                try:
-                    return f(*args, **kwargs)
-                except ExceptionToCheck, e:
-                    msg = "%s, Retrying in %d seconds..." % (str(e), mdelay)
-                    if logger:
-                        logger.exception(msg)
-                    else:
-                        print(msg)
-                    time.sleep(mdelay)
-                    mtries -= 1
-                    mdelay *= backoff
-                    mdelay = min(mdelay, max_delay)
-            return f(*args, **kwargs)
-
-        return f_retry  # true decorator
-
-    return deco_retry
-
-
-@retry((Exception, ), tries=30, logger=logger)
-def write_to_s3(bucket,
-                key_prefix,
-                tile,
-                tile_type,
-                data,
-                key_suffix,
-                headers,
-                overwrite=False):
+def s3_key(key_prefix, tile_type, tile, key_suffix):
     key = '{}/{}/{}/{}{}'.format(
         tile_type,
         tile.z,
         tile.x,
         tile.y,
-        key_suffix, )
+        key_suffix,
+    )
+
+    h = hashlib.md5(key).hexdigest()[:6]
+    key = '{}/{}'.format(
+        h,
+        key,
+    )
 
     if key_prefix:
         key = '{}/{}'.format(key_prefix, key)
 
-    obj = bucket.Object(key)
-    if overwrite:
-        obj.put(
-            Body=data,
-            ContentType=headers['Content-Type'],
-            Metadata={k: headers[k]
-                      for k in headers if k != 'Content-Type'})
-    else:
+    return key
+
+
+def write_to_s3(obj,
+                tile,
+                tile_type,
+                data,
+                key_suffix,
+                headers):
+
+    tries = 0
+    wait = 1.0
+    while True:
         try:
-            obj.load()
+            obj.put(
+                Body=data,
+                ContentType=headers['Content-Type'],
+                Metadata={k: headers[k]
+                          for k in headers if k != 'Content-Type'})
+
+            logger.debug(
+                "Saved tile %s to s3://%s/%s at try %s",
+                tile, obj.bucket_name, obj.key, tries,
+            )
+
+            return obj
         except botocore.exceptions.ClientError as e:
-            # If a client error is thrown, then check that it was a 404 error.
-            # If it was a 404 error, then the object does not exist.
-            error_code = int(e.response['Error']['Code'])
-            if error_code == 404:
-                obj.put(
-                    Body=data,
-                    ContentType=headers['Content-Type'],
-                    Metadata={
-                        k: headers[k]
-                        for k in headers if k != 'Content-Type'
-                    })
-
-    return obj
+            if e.response.get('Error', {}).get('Code') == 'SlowDown':
+                logger.info(
+                    "SlowDown received, try %s, while saving "
+                    "%s to s3://%s/%s, waiting %0.1f sec",
+                    tries, tile, obj.bucket_name, obj.key, wait,
+                )
+                time.sleep(wait)
+                wait = min(30.0, wait * 2.0)
+            else:
+                raise
 
 
-def build_source_index(tile):
-    source_cache = MemoryCatalog()
+def build_source_index(tile, min_zoom, max_zoom):
+    source_cache = MemoryAdapter()
     bbox = box(*mercantile.bounds(tile))
 
     database_url = os.environ.get('DATABASE_URL')
@@ -143,8 +145,15 @@ def build_source_index(tile):
                         wkb_geometry,
                         ST_GeomFromText(%s, 4326)
                     )
+                    AND numrange(min_zoom, max_zoom) && numrange(%s, %s)
                     AND enabled = true
-                """, (bbox.to_wkt(),))
+                """, (bbox.to_wkt(), min_zoom, max_zoom))
+
+            logger.info("Found %s sources for tile %s, zoom %s-%s",
+                cur.rowcount, tile, min_zoom, max_zoom)
+
+            if not cur.rowcount:
+                raise ValueError("No sources found for this tile")
 
             for row in cur:
                 row = dict(row)
@@ -162,49 +171,81 @@ def render_tile_exc_wrapper(tile, s3_details, sources):
         raise
 
 
-@retry((psycopg2.OperationalError, ), logger=logger)
-def render_tile(tile, format, transformation, sources):
-    return tiling.render_tile(
-        tile, sources, format=format, transformation=transformation)
+def s3_obj_exists(obj):
+    wait = 0.0
+
+    while True:
+        time.sleep(wait)
+
+        try:
+            obj.load()
+            return True
+        except botocore.exceptions.ClientError, e:
+            error_code = int(e.response['Error']['Code'])
+            if error_code == 404:
+                return False
+            else:
+                wait = min(30.0, wait * 2.0) if wait > 0 else 1.0
+                wait += random.uniform(0.0, wait / 2.0)
 
 
 def render_tile_and_put_to_s3(tile, s3_details, sources):
     s3_bucket, s3_key_prefix = s3_details
-    # Each thread needs its own boto3 Session object – it's not threadsafe
-    session = boto3.session.Session()
-    s3 = session.resource('s3')
-    bucket = s3.Bucket(s3_bucket)
 
-    for (type, transformation, format, ext) in RENDER_COMBINATIONS:
+    for (type, transformation, format, ext, scale) in RENDER_COMBINATIONS:
+        if ONLY_RENDER and type not in ONLY_RENDER:
+            logger.debug(
+                '(%02d/%06d/%06d) Skipping render because '
+                'type %s not in %s',
+                tile.z, tile.x, tile.y,
+                type, ONLY_RENDER,
+            )
+            continue
+
+        key = s3_key(s3_key_prefix, type, tile, ext)
+        obj = THREAD_LOCAL.s3.Object(s3_bucket, key)
+        if not OVERWRITE and s3_obj_exists(obj):
+            logger.debug(
+                '(%02d/%06d/%06d) Skipping existing %s tile',
+                tile.z, tile.x, tile.y, type,
+            )
+            continue
+
         with Timer() as t:
-            (headers, data) = render_tile(tile, format, transformation,
-                                          sources)
+            (headers, data) = tiling.render_tile(
+                tile, sources,
+                format=format,
+                transformation=transformation,
+                scale=scale)
 
-        logger.info(
+        logger.debug(
             '(%02d/%06d/%06d) Took %0.3fs to render %s tile (%s bytes), Source: %s, Timers: %s',
             tile.z, tile.x, tile.y, t.elapsed, type,
             len(data),
-            headers.get('X-Imagery-Sources'), headers.get('X-Timers'))
+            headers.get('X-Imagery-Sources'),
+            headers.get('X-Timers'),
+        )
 
         with Timer() as t:
-            obj = write_to_s3(bucket, s3_key_prefix, tile, type, data, ext,
-                              headers)
+            obj = write_to_s3(obj, tile, type, data, ext, headers)
 
-        logger.info('(%02d/%06d/%06d) Took %0.3fs to write %s tile to '
-                    's3://%s/%s', tile.z, tile.x, tile.y, t.elapsed, type,
-                    obj.bucket_name, obj.key)
+        logger.debug(
+            '(%02d/%06d/%06d) Took %0.3fs to write %s tile to s3://%s/%s',
+            tile.z, tile.x, tile.y, t.elapsed, type,
+            obj.bucket_name, obj.key,
+        )
 
 
-def queue_tile(tile, s3_details, sources):
+def queue_tile(tile, max_zoom, s3_details, sources):
     queue_render(tile, s3_details, sources)
 
-    if tile.z < MAX_ZOOM:
+    if tile.z < max_zoom:
         for child in mercantile.children(tile):
-            queue_tile(child, s3_details, sources)
+            queue_tile(child, max_zoom, s3_details, sources)
 
 
 def queue_render(tile, s3_details, sources):
-    logger.info('Enqueueing render for tile %s', tile)
+    logger.debug('Enqueueing render for tile %s', tile)
     POOL.apply_async(render_tile_exc_wrapper, args=[tile, s3_details, sources])
 
 
@@ -213,20 +254,24 @@ if __name__ == "__main__":
     parser.add_argument('x', type=int)
     parser.add_argument('y', type=int)
     parser.add_argument('zoom', type=int)
+    parser.add_argument('max_zoom', type=int)
     parser.add_argument('bucket')
-    parser.add_argument('key_prefix')
+    parser.add_argument('--key_prefix')
 
     args = parser.parse_args()
     root = Tile(args.x, args.y, args.zoom)
 
-    logger.info('Caching sources for root tile %s', root)
+    logger.info('Caching sources for root tile %s to zoom %s',
+                root, args.max_zoom)
 
-    source_index = build_source_index(root)
+    source_index = build_source_index(root, args.zoom, args.max_zoom)
 
     logger.info('Running %s processes', POOL_SIZE)
 
-    queue_tile(root, (args.bucket, args.key_prefix), source_index)
+    queue_tile(root, args.max_zoom, (args.bucket, args.key_prefix),
+               source_index)
 
     POOL.close()
     POOL.join()
-    logger.info('Done processing root pyramid %s', root)
+    logger.info('Done processing root pyramid %s to zoom %s',
+                root, args.max_zoom)
