e54eb99ac309f97dd1bb2a56c29e16aa02cabfb9
Seth Fitzsimmons <seth@mojodna.net>
2017-12-19 13:29:38 -0800

    Tools for mirroring requester-pays sources

---
+++ sentinel-2/mirror.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+source_bucket=$1
+source_prefix=$2
+target_bucket=$3
+
+aws s3api list-objects-v2 --bucket $source_bucket --prefix $source_prefix --delimiter / --request-payer requester | jq -r '.Contents[].Key' | parallel --no-notice -P 200% --eta aws s3api copy-object --request-payer requester --copy-source $source_bucket/{} --key $source_bucket/{} --bucket $target_bucket
